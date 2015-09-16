819b25b2a63da2c799882e0ed973f73c13ae04e4
Carl Caum <carl@carlcaum.com>
2015-09-16 09:08:14 -0700

    Properly build hosts file

--- satellite/scripts/install_satellite.sh
+++ satellite/scripts/install_satellite.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 if ! [ -d /opt/satellite ]; then
+  export dnsdomainname=`cat /etc/resolv.conf | grep domain | awk '{ print $2 }'`
+  export hostname=`hostname`
+  sed -i -- 's/^127.0.1.1.*//' /etc/hosts
+  sed -i -- "s/$hostname/$hostname.$dnsdomainname $hostname/" /etc/hosts
+
   mkdir -p /mnt/iso
   curl http://osmirror.delivery.puppetlabs.net/iso/satellite-6.0.4-rhel-7-x86_64-dvd.iso > /tmp/satellite-6.0.4-rhel-7-x86_64-dvd.iso
   mount /tmp/satellite-6.0.4-rhel-7-x86_64-dvd.iso -o loop /mnt/iso
