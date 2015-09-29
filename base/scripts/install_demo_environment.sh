30fdde3711aef0d7c6345d913f2a2f1b1708568a
Carl Caum <carl@carlcaum.com>
2015-09-29 12:07:06 -0700

    Properly get permissions to install required software

--- base/scripts/install_demo_environment.sh
+++ base/scripts/install_demo_environment.sh
@@ -9,13 +9,16 @@ fi
 
 [ -d $demo_pwd ] || git clone https://github.com/puppetlabs-pmmteam/pe-demo-env.git $demo_pwd
 
-which puppet || gem install puppet
+# Make sure we have sudo permissions
+echo "Type in your local password (What you use to log into you Mac):\n"
+sudo echo 'Installing system requirements......'
 
-which librarian-puppet || gem install librarian-puppet
+which puppet || sudo gem install puppet --no-rdoc --no-ri
+
+which librarian-puppet || sudo gem install librarian-puppet --no-rdoc --no-ri
 
 cd $demo_pwd/base/scripts
 librarian-puppet install
 cd $cwd
 
-echo "Type in your local password (What you use to log into you Mac):\n"
 sudo FACTER_username=$username puppet apply --modulepath $demo_pwd/base/scripts/modules $demo_pwd/base/scripts/demo_requirements.pp
