527e52499c072c5be6f4b87fb16e8efa363826d6
Carl Caum <carl@carlcaum.com>
2015-10-13 16:19:31 -0700

    Make post-install instructions clearer
    This commit makes the pots-install instructions clearer.  Previously, it was unclear to the user the pe-demo-env directory was created for them and that they needed to be in that directory for the demo environment to work.  This commit informs users about the directory and how to get to it.

--- base/scripts/install_demo_environment.sh
+++ base/scripts/install_demo_environment.sh
@@ -10,7 +10,7 @@ fi
 [ -d $demo_pwd ] || git clone https://github.com/puppetlabs-pmmteam/pe-demo-env.git $demo_pwd
 
 # Make sure we have sudo permissions
-echo "Type in your local password (What you use to log into you Mac):\n"
+echo "Type in your local password (What you use to log into you Mac):"
 sudo echo 'Installing system requirements......'
 
 which puppet || sudo gem install puppet --no-rdoc --no-ri
@@ -23,11 +23,15 @@ cd $cwd
 
 sudo FACTER_username=$username puppet apply --modulepath $demo_pwd/base/scripts/modules $demo_pwd/base/scripts/demo_requirements.pp
 
-cd $demo_pwd
 
 cat <<End-of-message
 -------------------------------------
 The demo environment should now be operational.
+
+A directory named pe-demo-env has been created
+for you. Go into that directory 
+with `cd pe-demo-env`,then you can use the 
+following commands to operate the environment.
 
 To see a list of available demo environments. run
   $ vagrant demo list
