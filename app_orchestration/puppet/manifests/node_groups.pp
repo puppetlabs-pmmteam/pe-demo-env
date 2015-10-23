06221e3de802838d2f4290064443468fec0156b9
Carl Caum <carl@carlcaum.com>
2015-10-23 12:22:52 -0700

    Dont manage classes in app host groups

--- app_orchestration/puppet/manifests/node_groups.pp
+++ app_orchestration/puppet/manifests/node_groups.pp
@@ -1,8 +1,6 @@
 node_group { 'Load Balancers':
   ensure               => 'present',
-  classes              => {
-    'profile::loadbalancer' => {}
-  },
+  classes              => { },
   environment          => 'production',
   rule                 => [ "=", [ "fact", "role" ], "loadbalancer" ],
   parent               => 'All Nodes',
@@ -10,9 +8,7 @@ node_group { 'Load Balancers':
 
 node_group { 'App Servers':
   ensure               => 'present',
-  classes              => {
-    'profile::appserver' => {}
-  },
+  classes              => { },
   environment          => 'production',
   rule                 => [ "=", [ "fact", "role" ], "appserver" ],
   parent               => 'All Nodes',
@@ -20,9 +16,7 @@ node_group { 'App Servers':
 
 node_group { 'Database Servers':
   ensure               => 'present',
-  classes              => {
-    'profile::database' => {}
-  },
+  classes              => { },
   environment          => 'production',
   rule                 => [ "=", [ "fact", "role" ], "database" ],
   parent               => 'All Nodes',
