58887f8552a94ae9a8aa7992e4df3c28e0ce50a2
Carl Caum <carl@carlcaum.com>
2015-10-28 21:13:28 -0700

    Set up the demo modules

---
+++ app_orchestration/puppet/manifests/demo_links.pp
@@ -0,0 +1,20 @@
+file { '/etc/puppetlabs/code/environments/production/manifests/site.pp':
+  ensure => symlink,
+  target => '/vagrant/app_orchestration/demo_site.pp',
+  force  => true,
+}
+
+file { '/etc/puppetlabs/code/modules/http':
+  ensure => symlink,
+  target => '/vagrant/app_orchestration/demo_modules/http',
+}
+
+file { '/etc/puppetlabs/code/modules/database':
+  ensure => symlink,
+  target => '/vagrant/app_orchestration/demo_modules/database',
+}
+
+file { '/etc/puppetlabs/code/modules/rgbank':
+  ensure => symlink,
+  target => '/vagrant/app_orchestration/demo_modules/rgbank',
+}
