a2d38ee5cc4709150bfede7344dea6f0ed546f38
Carl Caum <carl@carlcaum.com>
2015-10-29 14:28:20 -0700

    Do not clone rgbank app from github to PE master

--- app_orchestration/puppet/manifests/git_repos.pp
+++
@@ -1,9 +0,0 @@
-package { 'git':
-  ensure => present,
-}
-
-vcsrepo { '/var/lib/rgbank_app':
-  ensure   => present,
-  source   => 'https://github.com/puppetlabs-pmmteam/rgbank.git',
-  provider => git,
-}
