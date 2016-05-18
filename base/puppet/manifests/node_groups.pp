b6e109497d8e85e3c9518bc326f441a69230568a
Carl Caum <carl@carlcaum.com>
2016-05-18 12:15:24 -0700

    Update app orchestration configs

--- base/puppet/manifests/node_groups.pp
+++ base/puppet/manifests/node_groups.pp
@@ -1,11 +1,43 @@
+
+node_group { 'PE Infrastructure':
+  classes                            => {
+    'puppet_enterprise'              => {
+      'use_application_services'     => true,
+      'mcollective_middleware_hosts' => ["master"],
+      'database_host'                => "master",
+      'puppetdb_host'                => 'master',
+      'database_port'                => '5432',
+      'database_ssl'                 => true,
+      'puppet_master_host'           => 'master',
+      'certificate_authority_host'   => 'master',
+      'console_port'                 => '443',
+      'puppetdb_database_name'       => 'pe-puppetdb',
+      'puppetdb_database_user'       => 'pe-puppetdb',
+      'pcp_broker_host'              => 'master',
+      'puppetdb_port'                => '8081',
+      'console_host'                 => 'master'
+    },
+    environment => 'production',
+    require     => Exec['update classifier'],
+  }
+}
+
 node_group { 'PE Master':
-  classes              => {
+  classes                                              => {
     'pe_repo'                                          => {},
     'pe_repo::platform::el_7_x86_64'                   => {},
-    'puppet_enterprise::profile::master'               => {},
+    'pe_repo::platform::el_6_x86_64'                   => {},
+    'pe_repo::platform::debian_7_amd64'                => {},
+    'pe_repo::platform::debian_8_amd64'                => {},
+    'pe_repo::platform::ubuntu_1510_amd64'             => {},
+    'pe_repo::platform::ubuntu_1504_amd64'             => {},
+    'pe_repo::platform::windows_x86_64'                => {},
+    'puppet_enterprise::profile::master'               => {
+      'file_sync_enabled' => true,
+      'code_manager_auto_config' => true
+    },
     'puppet_enterprise::profile::master::mcollective'  => {},
     'puppet_enterprise::profile::mcollective::peadmin' => {},
-    'pe_r10k'                                          => { 'remote' => 'file:///var/lib/peadmin/site' }
   },
   environment          => 'production',
   parent               => 'PE Infrastructure',
